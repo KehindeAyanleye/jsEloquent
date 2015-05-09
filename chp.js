5922c5a931265e80061b37f3296028da3b8dfeff
Kehinde <kehindeayanleye@gmail.com>
2015-05-09 17:57:20 +0100

    run

---
+++ chp.js
@@ -0,0 +1,7 @@
+ar sizeFirm = Number(prompt("Choose a number", "0"));
+if (sizeFirm < 10)
+alert("it's a small firm of " + sizeFirm);
+else if (sizeFirm < 100)
+alert("it's a medium firm of " + sizeFirm);
+else
+alert("it's a large firm of " + sizeFirm)
