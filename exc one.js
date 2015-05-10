833b3adda51a6c86a26973cfb6cf4467a38d2e74
Kehinde <kehindeayanleye@gmail.com>
2015-05-10 02:59:49 +0100

    simple

---
+++ exc one.js
@@ -0,0 +1,5 @@
+var hashtag = "#"
+for(var count = 0; count < 7; count ++){
+console.log(hashtag);
+hashtag += "#";
+}
---
+++ fizzBuzz.js
@@ -0,0 +1,7 @@
+var all = 1;
+for( var number = 0; number <= 99; number ++){
+console.log(all);
+all ++;
+}
+%3 = "Fizz";
+%5 && !3 = "Buzz"
