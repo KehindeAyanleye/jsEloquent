95fc342ca9aff213fba5b1ef5bbd81fcc6c798e1
Kehinde <kehindeayanleye@gmail.com>
2015-05-10 02:06:02 +0100

    simple

---
+++ switch_intro.js
@@ -0,0 +1,15 @@
+// Run code here in the context of Chapter 2
+switch (prompt("what is the weather like today")){
+  case"rainy":
+		console.log("Hook up your rainy boot today");
+		break;
+  case"sunny":
+		console.log("Glue on to your latest sun shade all today");
+		break;
+  case"humid":
+		console.log("Don't give shi*s");
+	break;
+    default:
+		console.log("Unknown weather type");
+	break;
+  }
